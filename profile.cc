77cab48982454b34c3a0e304b2c41b70bb15740c
Siddarth Merchant <siddarthmerchant@outlook.com>
2019-05-15 20:51:02 -0700

    Complete 1st scenario

--- profile.cc
+++ profile.cc
@@ -1,5 +1,13 @@
 #include "profile.h"
+#include <cmath>
 
-const double Profile::GetTime() {
+const double Profile::GetTime(bool ignore_max_velocity) {
+  double distance = goal.position - current.position;
+
+  //senario #1, ignoring max velocity
+  if (ignore_max_velocity) {
+      return sqrt(2*distance/kMaxAcceleration);
+  }
+
   return 0.0;
 }
