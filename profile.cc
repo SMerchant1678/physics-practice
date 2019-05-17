89b3d2811a88f656487d846ac138ba659a0e65c2
Siddarth Merchant <siddarthmerchant@outlook.com>
2019-05-16 20:07:02 -0700

    add paranthasis

--- profile.cc
+++ profile.cc
@@ -6,7 +6,7 @@ const double Profile::GetTime(bool ignore_max_velocity) {
 
   //senario #1, ignoring max velocity
   if (ignore_max_velocity) {
-      return sqrt(2*distance/kMaxAcceleration);
+      return sqrt((2*distance)/kMaxAcceleration);
   }
 
   return 0.0;
